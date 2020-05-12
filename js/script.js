77471e209413aa33e9a81fbaed552036f12bc22e
Coopermakc <Coopermakc@mail.ru>
2020-05-12 10:33:40 +0300

    ru

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const textarea = document.querySelector("textarea");
 const button2 = document.querySelector(".save2");
 let boxes = document.querySelectorAll('input[type=checkbox]');
 
+
 function setCookie(name, value, options={}) {
   options = {
     path: '/'
@@ -31,49 +32,50 @@ function deleteCookie(name) {
   })
 }
 
+
 function checkCookie() {
-  [].map.call(boxes, function(el){
-    if (getCookie(el.id) != '' &&  getCookie(el.id) != undefined) {
-      el.checked = true
-    }
-  });
+    [].map.call(boxes, function(el){
+      if (getCookie(el.id) != '' &&  getCookie(el.id) != undefined) {
+        el.checked = true
+        el.addEventListener('click', () => {
+          el.checked = true;
+        })
+      }
+    });
 }
 
 button.onclick = () => {
-  let value = textarea.value;
-  if (value != "") {
-    this.setCookie("town", value);
-    document.location.reload();
-  } else {
-    alert('Vvedite gorod')
-  }
+    let value = textarea.value;
+    if (value != "") {
+      this.setCookie("town", value);
+      document.location.reload();
+    } else {
+      alert('Vvedite gorod')
+    }
 }
 
 button2.onclick = () => {
-  [].map.call(boxes, function(el){
-    if (el.checked) {
-      setCookie(el.id, el.id);
-    } else {
-      return !el.checked;
-    }
-  });
-  console.log(document.cookie);
+    [].map.call(boxes, function(el){
+      if (el.checked) {
+        setCookie(el.id, el.id);
+      } 
+    });
 }
 
 if (getCookie("town")) {
-  let town = getCookie("town");
-  let p = document.createElement("p");
-  let a = document.createElement("a");
-  p.textContent = `Your town is ${town}`;
-  a.textContent = `Delete town`;
-  a.href = "#";
-  document.querySelector('.text-form').style.display = 'none';
-  document.querySelector('.town-task').appendChild(p);
-  document.querySelector('.town-task').appendChild(a);
-  a.onclick = () => {
-    deleteCookie("town");
-    document.location.reload();
-  }
+    let town = getCookie("town");
+    let p = document.createElement("p");
+    let a = document.createElement("a");
+    p.textContent = `Ваш город - ${town}`;
+    a.textContent = `Изменить гоод`;
+    a.href = "#";
+    document.querySelector('.text-form').style.display = 'none';
+    document.querySelector('.town-task').appendChild(p);
+    document.querySelector('.town-task').appendChild(a);
+    a.onclick = () => {
+      deleteCookie("town");
+      document.location.reload();
+    }
 }
 
 checkCookie();
